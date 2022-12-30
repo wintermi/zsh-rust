f329732a6b21bc49635137862b33e7deb2ffbea1
Matthew Winter <33818+wintermi@users.noreply.github.com>
2022-12-30 21:40:07 +1100

    feat: Add zsh completions for 'rustup' and 'cargo'

--- zsh-rust.plugin.zsh
+++ zsh-rust.plugin.zsh
@@ -11,3 +11,16 @@ fi
 typeset -TUx PATH path
 path=("$HOME/.cargo/bin" $path)
 
+# Exit if the 'rustup' or 'cargo' commands can not be found
+if ! (( $+commands[rustup] && $+commands[cargo] )); then
+    echo "ERROR: 'rustup' or 'cargo' commands not found"
+    return
+fi
+
+# Completion directory for `rustup` and `cargo` commands once they have been generated
+ZSH_RUST_DIR="${0:A:h}/completions"
+rustup completions zsh >| "$ZSH_RUST_DIR/_rustup"
+cp -f "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" "$ZSH_RUST_DIR/_cargo"
+
+# Add completions to the FPATH
+fpath=("$ZSH_RUST_DIR" $fpath)
