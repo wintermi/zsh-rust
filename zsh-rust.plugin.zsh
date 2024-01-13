f0f7fcaac09aecf0ab042206e11f3e1d79f53508
Matthew Winter <33818+wintermi@users.noreply.github.com>
2024-01-13 22:24:25 +1100

    feat: modify the way the rustup and cargo completions are managed

--- zsh-rust.plugin.zsh
+++ zsh-rust.plugin.zsh
@@ -11,21 +11,35 @@ fi
 typeset -TUx PATH path
 path=("$HOME/.cargo/bin" $path)
 
-# Exit if the 'rustup', 'rustc' or 'cargo' command can not be found
+# Exit if the 'rustup', 'rustc' or 'cargo' commands can not be found
 if ! (( $+commands[rustup] && $+commands[rustc] && $+commands[cargo] )); then
-    echo "WARNING: 'rustup', 'rustc' or 'cargo' command not found"
+    echo "WARNING: 'rustup', 'rustc' or 'cargo' commands not found"
     return
 fi
 
 # Completions directory for `rustup` and `cargo` commands
 local COMPLETIONS_DIR="${0:A:h}/completions"
 
-# Only regenerate completions if older than 7 days, or does not exist
-if [[ ! -f "$COMPLETIONS_DIR/_rustup"  ||  ! $(find "$COMPLETIONS_DIR/_rustup" -newermt "7 days ago" -print) ]]; then
-    rustup completions zsh rustup >| "$COMPLETIONS_DIR/_rustup"
-    cat "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" >| "$COMPLETIONS_DIR/_cargo"
-fi
-
 # Add completions to the FPATH
 typeset -TUx FPATH fpath
 fpath=("$COMPLETIONS_DIR" $fpath)
+
+# If the completion file does not exist yet, then we need to autoload
+# and bind it to `rustup`. Otherwise, compinit will have already done that.
+if [[ ! -f "$COMPLETIONS_DIR/_rustup" ]]; then
+    typeset -g -A _comps
+    autoload -Uz _rustup
+    _comps[rustup]=_rustup
+fi
+
+# If the completion file does not exist yet, then we need to autoload
+# and bind it to `cargo`. Otherwise, compinit will have already done that.
+if [[ ! -f "$COMPLETIONS_DIR/_cargo" ]]; then
+    typeset -g -A _comps
+    autoload -Uz _cargo
+    _comps[cargo]=_cargo
+fi
+
+# Generate and load completion in the background
+rustup completions zsh rustup >| "$COMPLETIONS_DIR/_rustup" &|
+cat "$(rustc --print sysroot)/share/zsh/site-functions/_cargo" >| "$COMPLETIONS_DIR/_cargo" &|
